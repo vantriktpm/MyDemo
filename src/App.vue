0ef4bc208f692ac9ce25ea5ecc76b2cfb01fece9
admin <vantriktpm@gmail.com>
2022-12-11 21:02:48 +0700

    Change Layout

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="container">
-    <HeaderView />
-    <SliderView />
+  <!-- Page Preloder -->
+  <div id="preloder">
+    <div class="loader"></div>
   </div>
+
+  <HeaderView />
+  <SliderView />
+  <ContentView />
+  <FooterView />
 </template>
 
 <script>
 import HeaderView from './components/Header.vue'
 import SliderView from './components/Slider.vue'
+import ContentView from './components/Content.vue'
+import FooterView from './components/Footer.vue'
 
 export default {
   components: {
     HeaderView,
-    SliderView
-  }
+    SliderView,
+    ContentView,
+    FooterView
+  },
+  mounted() {
+    let main1 = document.createElement('script')
+    let main2 = document.createElement('script')
+    let main3 = document.createElement('script')
+    let main4 = document.createElement('script')
+    let main5 = document.createElement('script')
+    let main6 = document.createElement('script')
+    let main7 = document.createElement('script')
+    let main8 = document.createElement('script')
+    main1.setAttribute('src', 'js/jquery-3.3.1.min.js')
+    main2.setAttribute('src', 'js/bootstrap.min.js')
+    main3.setAttribute('src', 'js/player.js')
+    main4.setAttribute('src', 'js/jquery.nice-select.min.js')
+    main5.setAttribute('src', 'js/mixitup.min.js')
+    main6.setAttribute('src', 'js/jquery.slicknav.js')
+    main7.setAttribute('src', 'js/owl.carousel.min.js')
+    main8.setAttribute('src', 'js/main.js')
+    document.body.appendChild(main1)
+    document.body.appendChild(main2)
+    document.body.appendChild(main3)
+    document.body.appendChild(main4)
+    document.body.appendChild(main5)
+    document.body.appendChild(main6)
+    document.body.appendChild(main7)
+    document.body.appendChild(main8)
+  },
 }
 </script>
 
 <style>
-#app {
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: #0B0C2A;
-  width: max-width;
-}
 
-.container {
-  width: 100%;
-  background-color: #0B0C2A;
-}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,80 +1,49 @@
 <template>
-  <div class="header">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Categories
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr class="dropdown-divider">
+  <!-- Header Section Begin -->
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2">
+          <div class="header__logo">
+            <a href="./index.html">
+              <img src="img/logo.png" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="header__nav">
+            <nav class="header__menu mobile-menu">
+              <ul>
+                <li class="active"><a href="./index.html">Homepage</a></li>
+                <li><a href="./categories.html">Categories <span class="arrow_carrot-down"></span></a>
+                  <ul class="dropdown">
+                    <li><a href="./categories.html">Categories</a></li>
+                    <li><a href="./anime-details.html">Anime Details</a></li>
+                    <li><a href="./anime-watching.html">Anime Watching</a></li>
+                    <li><a href="./blog-details.html">Blog Details</a></li>
+                    <li><a href="./signup.html">Sign Up</a></li>
+                    <li><a href="./login.html">Login</a></li>
+                  </ul>
                 </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a href="./blog.html">Our Blog</a></li>
+                <li><a href="#">Contacts</a></li>
               </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-            
-            <li class="nav-item">
-              <a class="nav-link">About</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+            </nav>
+          </div>
+        </div>
+        <div class="col-lg-2">
+          <div class="header__right">
+            <a href="#" class="search-switch"><span class="icon_search"></span></a>
+            <a href="./login.html"><span class="icon_profile"></span></a>
+          </div>
         </div>
       </div>
-    </nav>
-  </div>
+      <div id="mobile-menu-wrap"></div>
+    </div>
+  </header>
+  <!-- Header End -->
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.header .container-fluid,
-.dropdown-menu {
-  background-color: #0B0C2A;
-}
-
-
-.navbar-toggler,
-.navbar-toggler-icon {
-  background-color: white;
-}
-
-a,
-.dropdown-item,
-.nav-item .disabled {
-  color: white;
-}
-
-.nav-link:hover {
-  color: orange;
-}
-
-.navbar-nav .nav-link.active{
-  color: orange;
-}
-
-.navbar-brand:hover {
-  color: orange;
-}
-
-.nav-link:focus, .navbar-brand:focus {
-  color: orange;
-}
 </style>
